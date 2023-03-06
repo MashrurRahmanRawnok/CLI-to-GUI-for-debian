2596f49ad46dcc3f4696d325c3f304992dd01447
RawnokRahman <127143435+RawnokRahman@users.noreply.github.com>
2023-03-07 01:28:52 +0600

    Add files via upload

---
+++ hacking-apps.sh
@@ -0,0 +1,98 @@
+sudo apt-get install gnome
+sudo update-alternatives --config x-session-manager
+wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
+echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
+sudo apt update
+sudo apt install anydesk
+sudo apt-get install beef-xss
+sudo add-apt-repository ppa:cyberfox-team/cyberfox-stable
+sudo apt update
+sudo apt install cyberfox
+sudo apt-get install bettercap
+sudo apt-get install bleachbit
+sudo apt-get install burpsuite
+sudo apt-get install crunch
+sudo apt-get install gimp
+sudo apt-get install snapd
+sudo snap install --classic code
+sudo apt-get install gparted
+sudo apt-get install hashcat
+sudo apt-get install torbrowser-launcher
+sudo apt-get install hydra
+sudo apt-get install openstego
+sudo apt-get install libreoffice
+sudo apt-get install recon-ng
+wget -O zoom.deb https://zoom.us/client/latest/zoom_amd64.deb
+sudo apt install ./zoom.deb
+sudo add-apt-repository ppa:app-outlet-team/ppa
+sudo apt-get update
+sudo apt-get install app-outlet
+sudo apt-get install aircrack-ng
+sudo apt-get install aircrack-ng
+sudo apt-get install aircrack-ng
+sudo apt-get install airscan
+sudo apt-get install airserv-ng
+sudo apt-get install airtun-ng
+sudo apt-get install airventriloquist-ng
+sudo apt-get install nmap
+sudo apt-get install wireshark
+sudo apt-get install metasploit-framework
+sudo apt-get install john
+sudo apt-get install hydra
+sudo apt-get install nikto
+sudo apt-get install dirbuster
+sudo apt-get install wpscan
+sudo apt-get install amap
+sudo apt-get install tcpdump
+sudo apt-get install hping3
+sudo apt-get install maltego
+sudo apt-get install openvas
+sudo apt-get install john-jumbo
+sudo apt-get install hash-identifier
+sudo apt-get install perl
+sudo apt-get install libwww-perl
+sudo apt-get install curl
+sudo apt-get install tor
+sudo apt-get install cpanminus
+sudo cpanm install Net::DNS
+sudo cpanm install IO::Socket::Socks
+sudo cpanm install Try::Tiny
+sudo cpanm install Config::Simple
+sudo cpanm install LWP::UserAgent
+sudo cpanm install JSON::MaybeXS
+sudo cpanm install Net::Netmask
+sudo curl -L https://cpanmin.us | perl - --sudo App::cpanminus
+sudo cpanm install Nipe
+sudo apt-get install vlc
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt-get install firefox
+sudo apt-get install libreoffice
+sudo snap install spotify
+sudo apt-get update
+wget https://zoom.us/client/latest/zoom_amd64.deb
+sudo dpkg -i zoom_amd64.deb
+sudo apt-get update
+sudo snap install slack --classic
+sudo apt-get update
+wget https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2020.03.04_amd64.deb
+sudo dpkg -i dropbox_2020.03.04_amd64.deb
+sudo apt-get install git
+sudo apt-get install nautilus
+sudo apt-get install thunar
+sudo apt-get install nemo
+sudo apt-get install dolphin
+sudo apt-get install pcmanfm
+sudo apt-get install eog
+sudo apt-get install gwenview
+sudo apt-get install nomacs
+sudo apt-get install openshot
+sudo apt-get install kdenlive
+sudo snap install shotcut --classic
+sudo apt-get update
+wget https://zoom.us/client/latest/zoom_amd64.deb
+sudo dpkg -i zoom_amd64.deb
+sudo apt-get install gdm
+sudo apt-get install lightdm
+sudo apt-get install sddm
+
